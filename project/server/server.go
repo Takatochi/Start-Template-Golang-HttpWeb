361cb4d453375c8107716126d5932d8c97233515
Arthur <79372730+Takatochi@users.noreply.github.com>
2022-08-10 17:41:14 +0300

    multicreateRefresh v.

---
+++ project/server/server.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func (s *Server) Run(port string, handler http.Handler) error {
+	s.httpServer = &http.Server{
+		Addr:           ":" + port,
+		MaxHeaderBytes: 1 << 20,
+		Handler:        handler,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+	}
+	return s.httpServer.ListenAndServe()
+
+}
