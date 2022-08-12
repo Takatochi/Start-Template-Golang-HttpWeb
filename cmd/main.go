317c65a9b47b66bbce355dc3e6819b92e859fc6c
Arthur <79372730+Takatochi@users.noreply.github.com>
2022-08-12 22:10:43 +0300

    add bd, refactorin

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"flag"
-	"html/template"
 	"log"
-	"project/pkg/handler"
 	"project/pkg/server"
-	"project/pkg/store"
 
 	"github.com/BurntSushi/toml"
-	"github.com/gin-gonic/gin"
 )
 
 var (
@@ -21,47 +17,26 @@ func init() {
 }
 
 func main() {
+	//server
+	srv := new(server.Server)
+
 	flag.Parse()
 
-	router := gin.New()
-	g := handler.InitHandler(router)
-
-	srv := new(server.Server)
+	// g := handler.NewHandler()
+	// routers := g.Routing()
 
 	config := server.NewConfig()
 	_, err := toml.DecodeFile(configPath, config)
 	if err != nil {
 		log.Fatal(err)
 	}
-	st := store.New(config)
 
-	if err := st.Open(); err != nil {
-		log.Fatal(err)
-	}
-	if err := Run(g, router); err != nil {
-		log.Fatal(err)
-	}
+	// if err := Run(g, routers); err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	if err := srv.Run(config, router); err != nil {
+	if err := srv.Run(config); err != nil {
 		log.Fatal(err)
 	}
 
-}
-func Run(hadler *handler.Handler, router *gin.Engine) error {
-
-	router.Static("/static", "./static/")
-
-	router.SetFuncMap(template.FuncMap{
-		"whole": handler.Whole,
-	})
-
-	router.LoadHTMLGlob("templates/*.html")
-
-	r(&hadler.Index, 2, router)
-	hadler.Contact.Routing(2, "contact", "/contact/", router)
-
-	return nil
-}
-func r(g handler.Routined, result any, router *gin.Engine) {
-	g.Routing(result, "index", "/", router)
 }
