d4fd129e60e05915b19e45f60a6a65228bce4ffb
Arthur <79372730+Takatochi@users.noreply.github.com>
2022-08-12 22:57:47 +0300

    Update userrepository_test.go
    Signed-off-by: Arthur <79372730+Takatochi@users.noreply.github.com>

--- pkg/store/sqlBd/userrepository_test.go
+++ pkg/store/sqlBd/userrepository_test.go
@@ -1,11 +1,19 @@
 package sqlBd_test
 
-// func TestUserRepository_Create(t *testing.T) {
-// 	db, teardown := sqlBd.TestDB(t, databaseURL)
-// 	defer teardown("user")
+import (
+	"project/pkg/model"
+	"project/pkg/store/sqlBd"
+	"testing"
 
-// 	s := sqlBd.New(db)
-// 	u := model.TestUser(t)
-// 	assert.NoError(t, s.User().Create(u))
-// 	assert.NotNil(t, u.ID)
-// }
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserRepository_Create(t *testing.T) {
+	db, teardown := sqlBd.TestDB(t, databaseURL)
+	defer teardown("user")
+
+	s := sqlBd.New(db)
+	u := model.TestUser(t)
+	assert.NoError(t, s.User().Create(u))
+	assert.NotNil(t, u.ID)
+}
